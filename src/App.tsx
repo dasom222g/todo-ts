6e170514c89865f7770f56dbc3721931c29fbcee
dasom <dasom228@gmail.com>
2021-02-19 18:09:37 +0900

    environment setting and exmaple practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Greetings from './Greetings';
 
-function App() {
+function App () {
+  function print (text: string): void {
+    console.log('text', text)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Greetings
+      name={'dasom'}
+      optional={'option입니다'}
+      print={print}
+    />
   );
 }
 
---
+++ src/Greetings.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+type GreetingProps = { // 이 컴포넌트의 props선언
+  name: string,
+  desc: string,
+  optional?: string,
+  print: (text: string) => void
+}
+
+// state
+
+function Greetings ({name, desc, optional, print}: GreetingProps) {
+  const handleClick = () => {
+    print(name)
+  }
+  return (
+    <div>
+      <div>Hello {name}!</div>
+      <strong>{desc}</strong>
+      {optional && <span>{optional}</span>}
+      <div>
+        <button type="button" onClick={handleClick}>Click me</button>
+      </div>
+    </div>
+  )
+}
+
+Greetings.defaultProps = {
+  desc: 'no description'
+}
+
+
+export default Greetings
